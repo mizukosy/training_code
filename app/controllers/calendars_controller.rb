a3d68282a2d1c1f0a6c7b0c9ecc486a128c797c5
mizukosy <kazukoshy@gmail.com>
2020-09-10 17:52:30 +0900

    issue6
    日付に対応した曜日へと修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -19,7 +19,6 @@ class CalendarsController < ApplicationController
   end
 
   def get_week
-    @today_week = Date.today.wday
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
@@ -35,13 +34,12 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = @today_week
-      if wday_num <= 7
-         wday_num -7
+      wday_num = Date.today.wday + x
+      if wday_num >= 7
+         wday_num = wday_num - 7
       end
-      days = { month:(@todays_date + x).month, date:(@todays_date+x).day, plans:today_plans, wdays:wdays[wday_num]}
+    days = { month:(@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wdays: wdays[wday_num]}
       @week_days.push(days)
     end
-
   end
 end
